43edea44e0f0c0f3e89d7649633e339ead4ebc2e
Ailrun <jjc9310@gmail.com>
2017-07-28 20:28:44 -0400

    Add license

--- coq-commenter.el
+++ coq-commenter.el
@@ -1,6 +1,21 @@
 ;;; coq-commenter.el --- Coq commenting minor mode for proof
 
-;; Version: 0.4.1
+;; Copyright (C) 2016-2017 Junyoung Clare Jang <jjc9310@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Version: 0.4.2
 ;; Keywords: comment coq proof
 ;; Author: Junyoung Clare Jang <jjc9310@gmail.com>
 ;; Maintainer: Junyoung Clare Jang <jjc9310@gmail.com>
